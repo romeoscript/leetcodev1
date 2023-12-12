5cbfdae31027f19b1ef3c10a725fed52868d3d84
Romanus Chukwuemeka <romeobourne211@gmail.com>
2023-12-12 21:20:08 +0100

    Time: 55 ms (30.88%), Space: 42.1 MB (31.01%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    for(i in arr) {
+        arr[i] = fn(arr[i], Number(i))
+    }
+        return arr
+};
